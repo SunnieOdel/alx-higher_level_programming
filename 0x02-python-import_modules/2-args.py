24cbbac3633e5ef9a527497fb0fe6a0caa4674c1
SunnieOdel <Sunnie_odel@yahoo.com>
2023-03-09 13:27:36 -0800

    Import Modules

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+if __name__ == "__main__":
+    """Print the number of and list of arguments."""
     import sys
 
     count = len(sys.argv) - 1
--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
+    """Print all names defined by hidden_4 module."""
     import hidden_4
-    for s in dir(hidden_4):
-        if s[:2] != "__":
-            print("{:s}".format(s))
+
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
