01874354eee93cb6ae094a0681d5d63135d9be7d
SunnieOdel <Sunnie_odel@yahoo.com>
2023-03-15 16:08:09 -0700

    More Data Structures

--- 0x04-python-more_data_structures/2-main.py
+++ 0x04-python-more_data_structures/2-main.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 uniq_add = __import__('2-uniq_add').uniq_add
 
 my_list = [1, 2, 3, 1, 4, 2, 5]
--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 def uniq_add(my_list=[]):
     number = 0
     for element in set(my_list):
